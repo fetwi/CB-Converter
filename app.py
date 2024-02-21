0fef93860f976f97111204801da69af61854387b
fetwi <fetwi.fitwi@gmail.com>
2024-02-21 14:55:27 -0500

    modified:   app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import streamlit as st
 
 st.set_page_config(
     page_title="Convertwi",
-    page_icon="favicon.ico",
+    page_icon="favicon.png",
     layout="wide",
     initial_sidebar_state="expanded",
     menu_items={
